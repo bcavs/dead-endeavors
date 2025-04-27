bef6a62068edf2186672792c31ad3944dbdc753a
Ben Cavenagh <cavenagh.ben@gmail.com>
2025-04-26 19:13:05 -0500

    add text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <Image
           src="/grateful_dead.svg"
           alt="Grateful Dead logo"
@@ -11,6 +11,9 @@ export default function Home() {
           height={535}
           priority
         />
+        <div className="flex flex-col gap-1 items-end">
+          <h1>Hi Mom! Hi Dad!</h1>
+        </div>
       </main>
     </div>
   );
